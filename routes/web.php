de8515e6777c22db7dc522994fb545837558918d
BangkitBayu <martinusmartinussaja@gmail.com>
2026-06-05 22:32:15 +0700

    style: setup layout template

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $title = 'Tukang Coretz | Home';
+    return view('index' , compact('title'))->name('home');
 });
+
+
