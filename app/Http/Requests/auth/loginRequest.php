6c69263c5d42e71d458111913b1302f9634c2298
BangkitBayu <martinusmartinussaja@gmail.com>
2026-06-05 14:13:14 +0700

    feat: add logic handle admin login request

---
+++ app/Http/Requests/auth/loginRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\auth;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Override;
+
+class loginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required'],
+            'password' => ['required']
+        ];
+    }
+
+    #[Override]
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'Invalid field',
+            'errors' => $validator->errors()
+        ], 422));
+    }
+}
