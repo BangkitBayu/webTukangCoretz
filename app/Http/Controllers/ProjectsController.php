d725a12b82ea6be62e73505a8e2cf3a1ea4aac4e
BangkitBayu <martinusmartinussaja@gmail.com>
2026-07-03 20:31:24 +0700

    feat: update form request store project

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -26,11 +26,6 @@ class ProjectsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create() {}
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(storeProjectRequest $request): RedirectResponse
